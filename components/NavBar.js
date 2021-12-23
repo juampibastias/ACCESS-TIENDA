e967db78612f1d466d1936faf56a7862e09f6bc5
juampibastias <juanpedrobastiastorresi@gmail.com>
2021-12-23 00:22:02 -0300

    primer push, web funcionando

---
+++ components/NavBar.js
@@ -0,0 +1,122 @@
+import React, { useContext } from 'react'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import {DataContext} from '../store/GlobalState'
+import Cookie from 'js-cookie'
+
+function NavBar() {
+    const router = useRouter()
+    const {state, dispatch} = useContext(DataContext)
+    const { auth, cart } = state
+
+
+    const isActive = (r) => {
+        if(r === router.pathname){
+            return " active"
+        }else{
+            return ""
+        }
+    }
+
+    const handleLogout = () => {
+        Cookie.remove('refreshtoken', {path: 'api/auth/accessToken'})
+        localStorage.removeItem('firstLogin')
+        dispatch({ type: 'AUTH', payload: {} })
+        dispatch({ type: 'NOTIFY', payload: {success: 'Logged out!'} })
+        return router.push('/')
+    }
+
+    const adminRouter = () => {
+        return(
+            <>
+            <Link href="/users">
+                <a className="dropdown-item">Usuario</a>
+            </Link>
+            <Link href="/create">
+                <a className="dropdown-item">Productos</a>
+            </Link>
+            <Link href="/categories">
+                <a className="dropdown-item">Categorias</a>
+            </Link>
+            </>
+        )
+    }
+
+    const loggedRouter = () => {
+        return(
+            <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src={auth.user.avatar} alt={auth.user.avatar} 
+                    style={{
+                        borderRadius: '50%', width: '30px', height: '30px',
+                        transform: 'translateY(-3px)', marginRight: '3px'
+                    }} /> {auth.user.name}
+                </a>
+
+                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <Link href="/profile">
+                        <a className="dropdown-item">Perfil</a>
+                    </Link>
+                    {
+                        auth.user.role === 'admin' && adminRouter()
+                    }
+                    <div className="dropdown-divider"></div>
+                    <button className="dropdown-item" onClick={handleLogout}>Salir</button>
+                </div>
+            </li>
+        )
+    }
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="text-center"  href="/">
+                <a className="navbar-brand text-center">ALMACEN online <br></br>
+                <img src='/images/rank.png' style={{width:"100px"}} className='p-2' alt="rank" /> 
+                <img src='/images/la-carniceria.png' style={{width:"100px"}} className='p-2' alt="carni" />
+                <img src='/images/laver.png' style={{width:"100px"}} className='p-2' alt="verdu" />
+                </a>
+                
+            </Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul className="navbar-nav p-1">
+                    <li className="nav-item">
+                        <Link href="/cart">
+                            <a className={"nav-link" + isActive('/cart')}>
+                                <i className="fas fa-shopping-cart position-relative" aria-hidden="true">
+                                    <span className="position-absolute"
+                                    style={{
+                                        padding: '3px 6px',
+                                        background: '#ed143dc2',
+                                        borderRadius: '50%',
+                                        top: '-10px',
+                                        right: '-10px',
+                                        color: 'white',
+                                        fontSize: '14px'
+                                    }}>
+                                        {cart.length}
+                                    </span>
+                                </i> Carrito
+                            </a>
+                        </Link>
+                    </li>
+                    {
+                        Object.keys(auth).length === 0 
+                        ? <li className="nav-item">
+                            <Link href="/signin">
+                                <a className={"nav-link" + isActive('/signin')}>
+                                    <i className="fas fa-user" aria-hidden="true"></i> Ingresar
+                                </a>
+                            </Link>
+                        </li>
+                        : loggedRouter()
+                    }
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ pages/register.js
@@ -0,0 +1,86 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {useState, useContext, useEffect} from 'react'
+import valid from '../utils/valid'
+import {DataContext} from '../store/GlobalState'
+import {postData} from '../utils/fetchData'
+import { useRouter } from 'next/router'
+
+
+const Register = () => {
+  const initialState = { name: '', email: '', password: '', cf_password: '' }
+  const [userData, setUserData] = useState(initialState)
+  const { name, email, password, cf_password } = userData
+
+  const {state, dispatch} = useContext(DataContext)
+  const { auth } = state
+
+  const router = useRouter()
+
+  const handleChangeInput = e => {
+    const {name, value} = e.target
+    setUserData({...userData, [name]:value})
+    dispatch({ type: 'NOTIFY', payload: {} })
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    const errMsg = valid(name, email, password, cf_password)
+    if(errMsg) return dispatch({ type: 'NOTIFY', payload: {error: errMsg} })
+
+    dispatch({ type: 'NOTIFY', payload: {loading: true} })
+
+    const res = await postData('auth/register', userData)
+    
+    if(res.err) return dispatch({ type: 'NOTIFY', payload: {error: res.err} })
+
+    return dispatch({ type: 'NOTIFY', payload: {success: res.msg} })
+  }
+
+  useEffect(() => {
+    if(Object.keys(auth).length !== 0) router.push("/")
+  }, [auth])
+
+    return(
+      <div>
+        <Head>
+          <title>Registrate en nuestro sitio</title>
+        </Head>
+
+        <form className="mx-auto my-4" style={{maxWidth: '500px'}} onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nombre</label>
+            <input type="text" className="form-control" id="name"
+            name="name" value={name} onChange={handleChangeInput} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+            name="email" value={email} onChange={handleChangeInput} />
+            <small id="emailHelp" className="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Contraseña</label>
+            <input type="password" className="form-control" id="exampleInputPassword1"
+            name="password" value={password} onChange={handleChangeInput} />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword2">Confirma tu contraseña</label>
+            <input type="password" className="form-control" id="exampleInputPassword2"
+            name="cf_password" value={cf_password} onChange={handleChangeInput} />
+          </div>
+          
+          <button type="submit" className="btn btn-dark w-100">Enviar</button>
+
+          <p className="my-2">
+            Ya tienes una cuenta? <Link href="/signin"><a style={{color: 'crimson'}}>Ingresar ahora</a></Link>
+          </p>
+        </form>
+      </div>
+    )
+  }
+  
+  export default Register
